5539534ef0748c99b5c58d2af72d3c0661431a04
Kamapra18 <marioprayoga18@gmail.com>
2026-05-21 21:51:26 +0800

    link DKV 1

--- src/data/events.ts
+++ src/data/events.ts
@@ -10,25 +10,25 @@ export const events: Event[] = [
   {
     slug: "DKV-1",
     name: "Event DKV Hari 1",
-    gift: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
+    gift: "https://drive.google.com/drive/folders/18_XD0CV7GWgc69Xn4T-mkHx2PyCl1p-e?usp=drive_link",
     frame:
-      "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
-    raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
+      "https://drive.google.com/drive/folders/1v4H9F5-7G0fmsy_xWK_w809-I39CfAlU?usp=drive_link",
+    raw: "https://drive.google.com/drive/folders/10gF0lPrXAbEWOJLeGzRKv1f-ZKM3P0r3?usp=drive_link",
   },
-  {
-    slug: "DKV-2",
-    name: "Event DKV Hari 2",
-    gift: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
-    frame:
-      "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
-    raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
-  },
-  {
-    slug: "DKV-3",
-    name: "Event DKV Hari 3",
-    gift: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
-    frame:
-      "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
-    raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
-  },
+  // {
+  //   slug: "DKV-2",
+  //   name: "Event DKV Hari 2",
+  //   gift: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
+  //   frame:
+  //     "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
+  //   raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
+  // },
+  // {
+  //   slug: "DKV-3",
+  //   name: "Event DKV Hari 3",
+  //   gift: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
+  //   frame:
+  //     "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
+  //   raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
+  // },
 ];
