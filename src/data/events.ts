68021bbd2a6e2cf3829f9a82176cf0889ac340a8
Kamapra18 <marioprayoga18@gmail.com>
2026-05-30 00:11:09 +0800

    refactor event structure and remove unused entries

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,35 +1,46 @@
 export type Event = {
+  id: number;
   slug: string;
   name: string;
-  video: string;
+  video?: string;
   frame: string;
-  raw: string;
+  raw?: string;
 };
 
 export const events: Event[] = [
   {
-    slug: "DKV-1",
-    name: "Event DKV Hari 1",
-    video:
-      "https://drive.google.com/drive/folders/18_XD0CV7GWgc69Xn4T-mkHx2PyCl1p-e?usp=drive_link",
+    id: 1,
+    slug: "STT-Presada",
+    name: "Ulang Tahun STT Presada",
     frame:
-      "https://drive.google.com/drive/folders/1v4H9F5-7G0fmsy_xWK_w809-I39CfAlU?usp=drive_link",
-    raw: "https://drive.google.com/drive/folders/10gF0lPrXAbEWOJLeGzRKv1f-ZKM3P0r3?usp=drive_link",
+      "https://drive.google.com/drive/folders/1WDnfb2Raz-q51SOow_TY9hdQM7kU59iz?usp=drive_link",
   },
-  // {
-  //   slug: "DKV-2",
-  //   name: "Event DKV Hari 2",
-  //   video: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
-  //   frame:
-  //     "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
-  //   raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
-  // },
-  // {
-  //   slug: "DKV-3",
-  //   name: "Event DKV Hari 3",
-  //   video: "https://drive.google.com/drive/folders/1oRI_6Ur8QefVyusAJskV2sEcIzxiVgIG?usp=sharing",
-  //   frame:
-  //     "https://drive.google.com/drive/folders/19Tc7doj256GRDyZU92JlOg38D1C3smqG?usp=sharing",
-  //   raw: "https://drive.google.com/drive/folders/1vvFOkY_JyYExyoYXfzY0ZWjtKKXEUD0I?usp=sharing",
-  // },
+  {
+    id: 2,
+    slug: "Krisna-&-Mika",
+    name: "Wedding Krisna & Mika",
+    frame:
+      "https://drive.google.com/drive/folders/1UHd4isEkKLVhyr8GW174t9zEXW7s6Vpf?usp=drive_link",
+  },
+  {
+    id: 3,
+    slug: "Yande-&-Melly",
+    name: "Wedding Yande & Melly",
+    frame:
+      "https://drive.google.com/drive/folders/1CdmLTpp3yvWASTLp94PVtnVCc72oxzyl?usp=drive_link",
+  },
+  {
+    id: 4,
+    slug: "Open-Campus-Primakara",
+    name: "Event Open Campus Primakara",
+    frame:
+      "https://drive.google.com/drive/folders/1JRH7ytfI4c95BsPhnjtKEBHsJTK_Ig2E?usp=drive_link",
+  },
+  {
+    id: 5,
+    slug: "Pameran-DKV-Primakara",
+    name: "Event Pameran DKV Primakara",
+    frame:
+      "https://drive.google.com/drive/folders/1gYMSQXmVqoYwauFNlS2t0BiEDOH5Q7bF?usp=drive_link",
+  },
 ];
