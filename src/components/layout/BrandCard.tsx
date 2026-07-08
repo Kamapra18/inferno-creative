6a70076282c2ca2b1d043820e8120429943e90d4
Kamapra18 <marioprayoga18@gmail.com>
2026-07-09 07:34:58 +0800

    update event title to Bali Startup Expo Primakara

--- src/components/layout/BrandCard.tsx
+++ src/components/layout/BrandCard.tsx
@@ -30,7 +30,7 @@ export default function CardLink() {
             letterSpacing: "0.02em",
           }}>
           Inferno Photobooth <br />
-          <span className="text-[16px]">Sitefest Primakara</span>
+          <span className="text-[16px]">Bali Startup Expo Primakara</span>
         </h1>
 
         <p
