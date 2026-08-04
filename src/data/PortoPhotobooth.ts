c84b428c4ec85466e16e7381dee76d416e07440a
Kamapra18 <marioprayoga18@gmail.com>
2026-08-04 21:09:55 +0800

    feat: update event details and add new events for wedding management

--- src/data/PortoPhotobooth.ts
+++ src/data/PortoPhotobooth.ts
@@ -15,22 +15,6 @@ export const portoEvents: Event[] = [
   // },
   {
     id: 1,
-    slug: "STT-Presada",
-    name: "Ulang Tahun STT Presada",
-    image: "/box.png",
-    frame:
-      "https://drive.google.com/drive/folders/1WDnfb2Raz-q51SOow_TY9hdQM7kU59iz?usp=drive_link",
-  },
-  {
-    id: 2,
-    slug: "Krisna-&-Mika",
-    name: "Wedding Krisna & Mika",
-    image: "/box.png",
-    frame:
-      "https://drive.google.com/drive/folders/1UHd4isEkKLVhyr8GW174t9zEXW7s6Vpf?usp=drive_link",
-  },
-  {
-    id: 3,
     slug: "Yande-&-Melly",
     name: "Wedding Yande & Melly",
     image: "/box.png",
@@ -38,29 +22,44 @@ export const portoEvents: Event[] = [
       "https://drive.google.com/drive/folders/1CdmLTpp3yvWASTLp94PVtnVCc72oxzyl?usp=drive_link",
   },
   {
+    id: 2,
+    slug: "Junet-&-Fenny",
+    name: "Wedding Junet & Fenny",
+    image: "/box.png",
+    frame:
+      "https://drive.google.com/drive/folders/1DuNzZpD5-Zc_77u91aSBDAZxvIm3i3JO?usp=drive_link",
+  },
+  {
+    id: 3,
+    slug: "Krisna-&-Mika",
+    name: "Wedding Krisna & Mika",
+    image: "/box.png",
+    frame:
+      "https://drive.google.com/drive/folders/1UHd4isEkKLVhyr8GW174t9zEXW7s6Vpf?usp=drive_link",
+  },
+  {
     id: 4,
-    slug: "Open-Campus-Primakara",
-    name: "Event Open Campus Primakara",
-    image: "/box.png",
-    frame:
-      "https://drive.google.com/drive/folders/1JRH7ytfI4c95BsPhnjtKEBHsJTK_Ig2E?usp=drive_link",
-  },
-  {
-    id: 5,
-    slug: "Pameran-DKV-Primakara",
-    name: "Event Pameran DKV Primakara",
-    image: "/box.png",
-    frame:
-      "https://drive.google.com/drive/folders/1gYMSQXmVqoYwauFNlS2t0BiEDOH5Q7bF?usp=drive_link",
-  },
-
-  {
-    id: 6,
     slug: "Ikumi-&-Cok",
     name: "Wedding Ikumi & Cok",
     image: "/box.png",
     frame:
       "https://drive.google.com/drive/folders/1Ho1HhUiU7e06X-oXE1bhW0Ykn_YEIJTt?usp=drive_link",
+  },
+  {
+    id: 5,
+    slug: "Bali-Startup-Expo-2026",
+    name: "Event Bali Startup Expo 2026",
+    image: "/box.png",
+    frame:
+      "https://drive.google.com/drive/folders/1Dgqm8F_LcAapJ2rn05tsb13R1jzk2nzE?usp=drive_link",
+  },
+  {
+    id: 6,
+    slug: "Open-Campus-Primakara",
+    name: "Event Open Campus Primakara",
+    image: "/box.png",
+    frame:
+      "https://drive.google.com/drive/folders/1JRH7ytfI4c95BsPhnjtKEBHsJTK_Ig2E?usp=drive_link",
   },
   {
     id: 7,
@@ -72,10 +71,18 @@ export const portoEvents: Event[] = [
   },
   {
     id: 8,
-    slug: "Bali-Startup-Expo-2026",
-    name: "Event Bali Startup Expo 2026",
+    slug: "Pameran-DKV-Primakara",
+    name: "Event Pameran DKV Primakara",
     image: "/box.png",
     frame:
-      "https://drive.google.com/drive/folders/1Dgqm8F_LcAapJ2rn05tsb13R1jzk2nzE?usp=drive_link",
+      "https://drive.google.com/drive/folders/1gYMSQXmVqoYwauFNlS2t0BiEDOH5Q7bF?usp=drive_link",
+  },
+  {
+    id: 9,
+    slug: "STT-Presada",
+    name: "Ulang Tahun STT Presada",
+    image: "/box.png",
+    frame:
+      "https://drive.google.com/drive/folders/1WDnfb2Raz-q51SOow_TY9hdQM7kU59iz?usp=drive_link",
   },
 ];
